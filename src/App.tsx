3fdbeb1604ced18699bde70c392bde6066a08036
jonatan timoteo dos santos <jonatan_ts@hotmail.com>
2022-07-14 11:29:42 -0300

    created routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
-import { GlobalStyles } from './components/styles/Global'
-import { defaultTheme } from './components/styles/themes/default'
+import { GlobalStyles } from './styles/Global'
+import { defaultTheme } from './styles/themes/default'
 import { Router } from './Router'
 
 export function App() {
